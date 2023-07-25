0b7025752b8a0c674275763f15974e6c21c76e7d
Donovan Glover <donovan@dglover.co>
2023-07-25 10:24:09 -0400

    Initial commit: v0.1.0
    This is a minimum viable product for hyprland-autodim that works for my
    use case but lacks any sort of customization for end-users.

---
+++ README.md
@@ -0,0 +1,36 @@
+# hyprland-autodim
+
+hyprland-autodim is a daemon that automatically dims windows temporarily when switching between them.
+
+## Advantages
+
+- Easy to see which window has focus, even with subtle or no borders.
+- Windows only dim when switching windows, eliminating the need to toggle dim on/off when you want to see other windows.
+
+## Usage
+
+```fish
+hyprland-autodim
+```
+
+## Contributing
+
+As far as I'm aware, this software is bug free. Although it uses `unsafe`, the actual code is rather succinct and easy to verify.
+
+That said, if you know how to do things better, feel free to open an issue or make a pull request. I'm particularly interested in alternatives that work without `unsafe`.
+
+## Todo
+
+- [ ] Turn `dim_inactive` on if it isn't already
+- [ ] Restore the original state of keywords when stopping the daemon
+- [ ] Add support for environment variables
+- [ ] Add support for command line arguments
+- [ ] Add man pages
+- [ ] Add shell completions
+- [ ] Add to [awesome-hyprland](https://github.com/hyprland-community/awesome-hyprland)
+- [ ] Add to [nixpkgs](https://github.com/NixOS/nixpkgs)
+
+## Thanks
+
+- [Yavor Kolev](https://github.com/yavko), [Cyril Levis](https://github.com/cyrinux), and [contributors](https://github.com/hyprland-community/hyprland-rs/graphs/contributors) for [hyprland-rs](https://github.com/hyprland-community/hyprland-rs)
+- [Kevin K.](https://github.com/kbknapp), [Ed Page](https://github.com/epage), and [contributors](https://github.com/clap-rs/clap/graphs/contributors) for [clap-rs](https://github.com/clap-rs/clap)
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use hyprland::event_listener::EventListenerMutable as EventListener;
+use hyprland::keyword::*;
+use hyprland::shared::Address;
+use std::{thread, time};
+
+// (1): Keep track of how many threads are running
+static mut I: i32 = 0;
+
+// (2): Keep track of the last window address
+static mut ADDRESS: Option<Address> = None;
+
+fn dim() {
+    // Note tha dim_strength is used instead of toggling dim_inactive for smooth animations
+    let _ = Keyword::set("decoration:dim_strength", 0.25);
+
+    unsafe {
+        // (1): Wait X milliseconds, keeping track of the number of waiting threads with I
+        I += 1;
+        thread::sleep(time::Duration::from_millis(800));
+        I -= 1;
+
+        // (1): If this is the last thread, remove dim
+        if I == 0 {
+            let _ = Keyword::set("decoration:dim_strength", 0);
+        }
+    }
+}
+
+fn is_new_window(window_address: Address) -> bool {
+    let mut windows_are_the_same = false;
+
+    unsafe {
+        match &ADDRESS {
+            // (2): If the saved address is the same as the new window, they're the same
+            Some(address) => {
+                let old_address = format!("{:?}", address.clone());
+                let new_address = format!("{:?}", window_address.clone());
+
+                if old_address == new_address {
+                    windows_are_the_same = true;
+                }
+            }
+
+            // (2): Fallback for when an initial address hasn't been saved yet
+            None => {}
+        }
+        ADDRESS = Some(window_address);
+    }
+
+    !windows_are_the_same
+}
+
+fn main() -> hyprland::Result<()> {
+    let mut event_listener = EventListener::new();
+
+    // On active window changes
+    event_listener.add_active_window_change_handler(|data, _| {
+        let Some(hyprland::event_listener::WindowEventData { window_address, .. }) = data else {
+            // Ignore the event if no window_address was given
+            return
+        };
+
+        // Only dim if the active window is a new window
+        if is_new_window(window_address) {
+            let _ = thread::spawn(|| dim());
+        }
+    });
+
+    event_listener.start_listener()
+}
