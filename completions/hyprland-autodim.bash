36bde5350089d4a6017ca692f516ac4133745621
Donovan Glover <donovan@dglover.co>
2023-07-25 21:26:33 -0400

    meta: Add generated completions to repo

---
+++ completions/hyprland-autodim.bash
@@ -0,0 +1,70 @@
+_hyprland-autodim() {
+    local i cur prev opts cmd
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${cmd},${i}" in
+            ",$1")
+                cmd="hyprland__autodim"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        hyprland__autodim)
+            opts="-s -d -f -b -h --strength --duration --fade --bezier --help"
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                --strength)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -s)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --duration)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -d)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --fade)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -f)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --bezier)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -b)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+    esac
+}
+
+complete -F _hyprland-autodim -o bashdefault -o default hyprland-autodim
---
+++ completions/hyprland-autodim.fish
@@ -0,0 +1,5 @@
+complete -c hyprland-autodim -s s -l strength -d 'A value from 0 (no dim) to 1 (maximum dim)' -r
+complete -c hyprland-autodim -s d -l duration -d 'How many milliseconds to wait' -r
+complete -c hyprland-autodim -s f -l fade -d 'Fade animation speed' -r
+complete -c hyprland-autodim -s b -l bezier -d 'Bezier curve used for the animation' -r
+complete -c hyprland-autodim -s h -l help -d 'Print help'
