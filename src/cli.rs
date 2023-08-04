07cbeee8d9bbb92926c743a7592b176408644e15
Donovan Glover <donovan@dglover.co>
2023-08-04 14:16:53 -0400

    feat: Add --no-dim-when-only option
    The implementation for this is complex, which is why I decided to
    implement it across multiple separate commits.

--- src/cli.rs
+++ src/cli.rs
@@ -54,6 +54,22 @@ pub struct Cli {
     #[arg(short, long, default_value_t = false)]
     pub persist: bool,
 
+    /// Don't dim when switching to a workspace that only has one window
+    ///
+    /// Usually when you use dim in Hyprland, windows are dimmed even when switching between
+    /// workspaces. This can be annoying if you constantly switch between two different workspaces
+    /// with one window.
+    ///
+    /// Note that this option also takes into account whether a window is fullscreen or not since
+    /// a workspace with multiple windows but an active one fullscreen makes dimming redundant.
+    ///
+    /// Note that this option also doesn't dim when switching between a special workspace that has
+    /// only one window. In the future if Hyprland supports fullscreen windows inside special
+    /// workspaces, the codebase will need to be updated as appropriate. Please file an issue or
+    /// make a pull request when this occurs.
+    #[arg(short, long, default_value_t = false)]
+    pub no_dim_when_only: bool,
+
     /// Show information about what hyprdim is doing
     #[arg(short, long, default_value_t = false)]
     pub verbose: bool,
