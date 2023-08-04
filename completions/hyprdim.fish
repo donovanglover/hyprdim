4eb772193cd9ce3b971d801fafdba59b506f87a7
Donovan Glover <donovan@dglover.co>
2023-08-04 11:22:18 -0400

    chore: Update shell completions

--- completions/hyprdim.fish
+++ completions/hyprdim.fish
@@ -1,6 +1,6 @@
 complete -c hyprdim -s s -l strength -d 'A value from 0 (no dim) to 1 (maximum dim)' -r
-complete -c hyprdim -s d -l duration -d 'How many milliseconds to wait' -r
-complete -c hyprdim -s f -l fade -d 'Fade animation speed' -r
+complete -c hyprdim -s d -l duration -d 'How many milliseconds to wait before removing dim' -r
+complete -c hyprdim -s f -l fade -d 'Fade animation speed from 0 (instantaneous) to 255 (very slow)' -r
 complete -c hyprdim -s b -l bezier -d 'Bezier curve used for the animation' -r
 complete -c hyprdim -s p -l persist -d 'Prevent dim_inactive from being disabled by `hyprctl reload` etc'
 complete -c hyprdim -s v -l verbose -d 'Show information about what hyprdim is doing'
