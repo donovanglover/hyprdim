97a34658025726efc11f338b9cb2f774fa06350a
Donovan Glover <donovan@dglover.co>
2023-08-06 08:32:15 -0400

    tests: Compare Cargo.toml version with PKGBUILD

--- tests/release.rs
+++ tests/release.rs
@@ -1,5 +1,16 @@
 use rustympkglib::pkgdata::PkgData;
 use std::fs;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    package: Option<PackageConfig>,
+}
+
+#[derive(Debug, Deserialize)]
+struct PackageConfig {
+    version: Option<String>,
+}
 
 #[test]
 /// We have the version number in quite a few places. This test ensures that all
@@ -9,7 +20,9 @@ fn versions_are_the_same() {
     let pkgbuild = PkgData::from_source(pkgbuild).unwrap();
     let pkgbuild = pkgbuild.pkgver;
 
-    // let cargo = &fs::read_to_string("Cargo.toml").unwrap();
-    // let cargo = TODO: Parse toml
-    assert_eq!(pkgbuild, "2.0.1");
+    let cargo = &fs::read_to_string("Cargo.toml").unwrap();
+    let cargo: Config = toml::from_str(cargo).unwrap();
+    let cargo = cargo.package.unwrap().version.unwrap();
+
+    assert_eq!(pkgbuild, cargo.as_str());
 }
