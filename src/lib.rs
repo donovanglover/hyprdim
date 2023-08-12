donovanglover/hyprdim

commit ce8183c111b9c5e0c5012d46ab1de37c1e0272c0

lib: Add is_special()
Unfortunately there doesn't seem to be a way to get the data of a
specific workspace, which could be used to get the number of open
windows on a workspace.

src/lib.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use cli::Cli;
+use hyprland::data::Client;
 use hyprland::keyword::Keyword;
+use hyprland::prelude::*;
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
@@ -44,4 +46,13 @@ pub fn spawn_dim_thread(num_threads: Arc<Mutex<u16>>, strength: f64, persist: bo
 
         Ok(())
     });
+}
+
+/// Gets whether the current workspace is a special workspace or not.
+///
+/// This function works by getting which workspace the active window is in.
+pub fn is_special() -> bool {
+    let Client { workspace, .. } = Client::get_active().unwrap().unwrap();
+
+    workspace.name.contains("special")
 }

commit cf81b4ed43cd1340c9d5c9c3796de1f3bd59f876

fix(is_special): Ensure that active window exists
This fixes an issue where hyprdim would panic if started with no windows
in the active workspace.

This bug was introduced in ce8183c111b9c5e0c5012d46ab1de37c1e0272c0,
which means that only v2.1.0 of hyprdim is affected.

Resolves #4.

src/lib.rs
@@ -57,10 +57,16 @@ pub fn spawn_dim_thread(
 /// Gets whether the current workspace is a special workspace or not.
 ///
 /// This function works by getting which workspace the active window is in.
+///
+/// The if statement is used to make sure this function works when no window
+/// is the active window.
 pub fn is_special() -> bool {
-    let Client { workspace, .. } = Client::get_active().unwrap().unwrap();
+    if let Some(client) = Client::get_active().unwrap() {
+        let Client { workspace, .. } = client;
+        return workspace.name.contains("special");
+    }
 
-    workspace.name.contains("special")
+    false
 }
 
 /// Returns true if there is only one visible window in the special workspace.