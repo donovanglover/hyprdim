4b0397d87e659830e438fdd0b488e5545db8d1e9
Donovan Glover <donovan@dglover.co>
2024-10-16 06:25:28 -0400

    feat!: check fullscreen for is_single

--- src/queries/is_single.rs
+++ src/queries/is_single.rs
@@ -2,11 +2,6 @@ use hyprland::data::{Client, WorkspaceBasic, Workspaces};
 use hyprland::prelude::*;
 
 /// Returns true if there is only one visible window in the active workspace.
-///
-/// In the future, this function should be updated to accommodate for fullscreen
-/// windows in special workspaces if Hyprland implements it.
-///
-/// https://github.com/hyprwm/Hyprland/issues/2173
 pub fn is_single() -> bool {
     if let Some(client) = Client::get_active().unwrap() {
         let Client { workspace, .. } = client;
@@ -14,7 +9,7 @@ pub fn is_single() -> bool {
 
         for workspace in Workspaces::get().unwrap() {
             if workspace.id == id {
-                return workspace.windows == 1;
+                return workspace.windows == 1 || workspace.fullscreen;
             }
         }
     }
