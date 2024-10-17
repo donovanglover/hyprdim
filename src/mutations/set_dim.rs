e107c1bc991198ae75bfc3bba89a827b516e2219
Donovan Glover <donovan@dglover.co>
2024-10-17 19:23:53 -0400

    chore: clean up set_dim docs

--- src/mutations/set_dim.rs
+++ src/mutations/set_dim.rs
@@ -1,15 +1,11 @@
 use hyprland::keyword::Keyword;
 
-use crate::utils::log;
-
-/// Sets the dim strength to a specific value permanently until it gets changed again.
+/// Sets the dim strength to a specific value
 ///
-/// Useful for setting the dim of dialog windows.
+/// Always sets `dim_inactive` to ensure that changes are applied.
 pub fn set_dim(strength: f64) -> hyprland::Result<()> {
     Keyword::set("decoration:dim_inactive", "yes")?;
     Keyword::set("decoration:dim_strength", strength)?;
-
-    log("info: Set a permanent dim (until next event) without spawning thread");
 
     Ok(())
 }
